b8f81198f856868262ddaf2f270e4e90669b3601
CherryStopher <cristobals80@hotmail.com>
2024-10-11 04:17:07 -0300

    allow cros connections

--- back/eduapi/settings.py
+++ back/eduapi/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+#
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = "eduapi.urls"
